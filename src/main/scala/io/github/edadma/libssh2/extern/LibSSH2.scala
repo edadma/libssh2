10f5e81631ab914507b20939825116e937ed8c44
Edward A Maxedon <edadma@gmail.com>
2022-12-22 19:07:44 -0500

    externs

--- src/main/scala/io/github/edadma/libssh2/extern/LibSSH2.scala
+++ src/main/scala/io/github/edadma/libssh2/extern/LibSSH2.scala
@@ -6,6 +6,13 @@ import scala.scalanative.unsafe._
 @extern
 object LibSSH2:
   type session_t = CStruct0
+  type session_tp = Ptr[session_t]
 
   def libssh2_init(flags: CInt): CInt = extern // 530
   def libssh2_exit(): Unit = extern // 537
+  def libssh2_session_init_ex(
+      my_alloc: Ptr[CChar],
+      my_free: Ptr[CChar],
+      my_realloc: Ptr[CChar],
+      abstrct: Ptr[CChar],
+  ): session_tp = extern // 562
--- src/main/scala/io/github/edadma/libssh2/package.scala
+++ src/main/scala/io/github/edadma/libssh2/package.scala
@@ -2,5 +2,8 @@ package io.github.edadma.libssh2
 
 import extern.{LibSSH2 => lib}
 
+implicit class Session(val surface: lib.session_tp)
+
 def init(flags: Int): Int = lib.libssh2_init(flags)
 def exit(): Unit = lib.libssh2_exit()
+def sessionInit: Session = lib.libssh2_session_init_ex(null, null, null, null)
